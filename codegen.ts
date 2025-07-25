ecac11d701503c2c2df168f0ea0d20af546b32b5
nurtilekkamalov <101614562+nurtilekkamalov@users.noreply.github.com>
2025-07-25 23:05:17 +0600

    Update codegen.ts

--- codegen.ts
+++ codegen.ts
@@ -1,11 +1,10 @@
 import { CodegenConfig } from "@graphql-codegen/cli";
 const { loadEnvConfig } = require("@next/env");
-
 loadEnvConfig(process.cwd());
 
 const config: CodegenConfig = {
-  schema: "https://worldnomad.iprofiit.pro/graphql", // ← исправлено: добавлены кавычки
-  documents: ["src/**/*.tsx", "src/**/*.ts"], // ← всё ОК
+  schema: process.env.NEXT_PUBLIC_WORDPRESS_URL,
+  documents: ["src/**/*.tsx", "src/**/*.ts"], // <--- ВОТ ВАЖНОЕ ИЗМЕНЕНИЕ
   generates: {
     "./src/__generated__/": {
       preset: "client",
