b8a21ee95710c9e0f4dd6bdce5d15dd46f845d4e
nurtilekkamalov <101614562+nurtilekkamalov@users.noreply.github.com>
2025-07-25 23:10:36 +0600

    Update codegen.ts

--- codegen.ts
+++ codegen.ts
@@ -1,14 +1,19 @@
 import { CodegenConfig } from "@graphql-codegen/cli";
 const { loadEnvConfig } = require("@next/env");
+
 loadEnvConfig(process.cwd());
 
 const config: CodegenConfig = {
-  schema: process.env.NEXT_PUBLIC_WORDPRESS_URL,
-  documents: ["src/**/*.tsx", "src/**/*.ts"], // <--- ВОТ ВАЖНОЕ ИЗМЕНЕНИЕ
+  schema: `${process.env.NEXT_PUBLIC_WORDPRESS_URL}/graphql`,
+  documents: ["src/**/*.ts", "src/**/*.tsx"],
   generates: {
     "./src/__generated__/": {
       preset: "client",
-      plugins: [],
+      plugins: [
+        "typescript",
+        "typescript-operations",
+        "typescript-react-apollo",
+      ],
       presetConfig: {
         gqlTagName: "gql",
       },
